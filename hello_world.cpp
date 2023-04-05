93043a5f98e4307d634918d14fbaf5c384549364
t1mlY <kutiro56@gmail.com>
2023-04-06 01:35:58 +0300

    Again Hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 int main() {
-   std::string Name;
-   std::cin>>Name;
-    std::cout << "Hello world"<<Name<<std::endl;
+    std::cout << "Hello world"<<std::endl;
     return 0;
 }
 
